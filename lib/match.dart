56aef9bbb0996c50c14d5f39413f9157b5d0913d
Josiah <josiahmutinda99@gmail.com>
2023-05-13 16:54:28 -0700

    match

---
+++ lib/match.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Match extends StatelessWidget {
+  const Match({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              width: 300,
+              height: 100,
+              child: Row(
+                children: [
+                  Container(
+                    width: 300,
+                    height: 100,
+                    child: ,
+                  )
+                ],
+              )
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
